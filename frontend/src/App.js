2f7b868a405d6f8f02486832656f750df20f2446
Luis Rodriguez <luis@missionreadyhq.com>
2023-09-16 06:54:25 +1200

    commit before fetching development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-// src/App.js
 import React from "react";
-import MessageForm from "./components/MessageForm";
+// import MessageForm from "./components/MessageForm";
 
 function App() {
   return (
     <div className="App">
-      <MessageForm />
+      {/* <MessageForm /> */}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <span class="text-teal-600 bg-teal-500">Admin</span>
     </div>
   );
 }
